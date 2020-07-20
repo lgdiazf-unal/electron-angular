ffaf0d4f5c6c5edef52e8ba77b94271b61457f6d
lgdiazf <lgdiazf1@gmail.com>
2020-07-20 08:57:44 -0500

    dev angular

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app,BrowserWindow} = require('electron');
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
+        width : 400,
+        height : 600,
+        backgroundColor:'#ffffff',
+        webPreferences: {
+            nodeIntegration: true
+          }
+    })
+
+    win.loadURL(
+        isDev ? "http://localhost:4200/" :
+        `file://${path.join(__dirname,"dist","electron-angular","index.html")}`
+    );
+
+    //wind.webContents.openDevTools();
+
+    win.on('close',function(){
+        win = null
+    })
+}
+app.on('ready',createWindow);
+app.on('window-all-closed',function(){
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+app.on('activate',function(){
+    if (win === null){
+        createWindow()
+    }
+})
